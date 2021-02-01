ee3ddd05ce784f0d3f306fcabbedd692d64e2f32
MaximPekshev <mp198769@gmail.com>
2021-02-01 13:40:37 +0300

    allowed hosts

--- negrustno/settings.py
+++ negrustno/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['negrustno.ru', 'www.negrustno.ru', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
